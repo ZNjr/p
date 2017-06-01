1f9e0219567dfe96e98a245fe2adac5e8fc693f7
ZNjr <znajer@gmail.com>
2017-06-01 09:34:56 +0200

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,9 +1,44 @@
 class TeamStats:
-    players = []
-    goals = []
-    cards = []
-    subs = []
-    score = 0
-
     def __init__(self):
-        pass
+        self.score = 0
+        self.name = ""
+        self.stats = {'players': [], 'subs': [], 'goals': [], 'cards': []}
+
+    def players(self):
+        print("PLAYERS:")
+        for player in self.stats['players']:
+            print(player)
+
+    def addGoal(self, goal):
+        wasPlayer = 0
+        for g in self.stats['goals']:
+            if goal['player'] in g['player']:
+                g['minute'].append(goal['minute'][0])
+                g['og'].append(goal['og'][0])
+                wasPlayer = 1
+                break
+        if not wasPlayer:
+            self.stats['goals'].append(goal)
+
+    def get_goals(self):
+        print("GOALS:")
+        for goal in self.stats['goals']:
+            result = ''
+            for minute in goal['minute']:
+                result += minute + "' "
+            result += goal['player']
+            print(result)
+
+    def get_cards(self):
+        print("CARDS:")
+        for card in self.stats['cards']:
+            print(card['minute'] + "' " + card['player'] + " " + card['color'])
+
+    def get_subs(self):
+        print("SUBS:")
+        for sub in self.stats['subs']:
+            print(sub['minute'] + "' " + sub['in'] + " for " + sub['out'])
+
+    def get_score(self):
+        print(self.score)
+
