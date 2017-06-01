27db897b0818ff74f87dbc8100db025efd60d06c
ZNjr <znajer@gmail.com>
2017-06-01 17:58:16 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -6,6 +6,13 @@ def isOwnGoal(raportLine):
     return re.search(r"\(og\)", raportLine, re.I)
 
 
+def cardColor(card):
+    if re.match(r"red|r", card, re.I):
+        return "R"
+    else:
+        return "Y"
+
+
 class GameParser:
     headParser = re.compile(
         r"^(?:((?:[a-z]+ ){1,3})(?:vs|v.|) ((?:[a-z]+ ){1,3})(?:@|venue|at) ((?:[a-z]+ )+)on ([1-9][0-9]? (?:(?:[a-z]+)|(?:[1-9][0-9])) \d{4}))$",
@@ -17,13 +24,15 @@ class GameParser:
 
     goalParser = re.compile(r"^(" + minute + " +(?:g+o+a+l+).*)$", re.M | re.I)
     cardParser = re.compile(r"^(" + minute + " +(?:card +(yellow|y|red|r)).*)$", re.M | re.I)
-    subParser = re.compile(r"^(" + minute + " +(?:substitution|sub) +(?:out|off)((?: +[a-z.]+)+) +(?:in|on)((?: +[a-z.]+)+))",
-                           re.M | re.I)
+    subParser = re.compile(
+        r"^(" + minute + " +(?:substitution|sub) +(?:out|off)((?: +[a-z.]+)+) +(?:in|on)((?: +[a-z.]+)+))",
+        re.M | re.I)
 
     def __init__(self, team1, team2):
         self.team1 = team1
         self.team2 = team2
         self.head = {'home': "", 'away': "", 'scores': "", 'venue': "", 'data': ""}
+        self.parsed = 0
 
     def parse(self, raport):
         header = self.headParser.findall(raport)
@@ -36,6 +45,9 @@ class GameParser:
         self.insertGoalsStats(gools)
         cards = self.cardParser.findall(raport)
         self.insertCardsStats(cards)
+        self.parsed = 1
+        self.team1.parse = 1
+        self.team2.parse = 1
 
     def insertPlayersData(self, players):
         ct = 0
@@ -48,16 +60,16 @@ class GameParser:
 
     # teamOfFoundPlayer return (team , player)
     def teamOfFoundPlayer(self, raportLine):
-        for player in team1.stats['players']:
+        for player in self.team1.stats['players']:
             if player in raportLine:
                 return {'team': self.team1, 'player': player}
-        for player in team2.stats['players']:
+        for player in self.team2.stats['players']:
             if player in raportLine:
                 return {'team': self.team2, 'player': player}
-        for sub in team1.stats['subs']:
+        for sub in self.team1.stats['subs']:
             if sub['in'] in raportLine:
                 return {'team': self.team1, 'player': sub['in']}
-        for sub in team2.stats['subs']:
+        for sub in self.team2.stats['subs']:
             if sub['in'] in raportLine:
                 return {'team': self.team2, 'player': sub['in']}
 
@@ -76,28 +88,31 @@ class GameParser:
             playerDate = self.teamOfFoundPlayer(goal[0])
             player = playerDate['player']
             team = playerDate['team']
+            ogTeam = self.team1
+            if team is self.team1:
+                ogTeam = self.team2
             if isOwnGoal(goal[0]):
-                if team is team1:
+                if team is self.team1:
                     away = away + 1
-                if team is team2:
+                if team is self.team2:
                     home = home + 1
-                team.addGoal({'player': player, 'minute': [goal[1]], 'og': [1]})
+                ogTeam.addGoal({'player': player, 'minute': [goal[1]], 'og': [1]})
             else:
-                if team is team1:
+                if team is self.team1:
                     home = home + 1
-                if team is team2:
+                if team is self.team2:
                     away = away + 1
-                team.addGoal({'player': player, 'minute': [goal[1]], 'og': [0]})
+                team.addGoal({'player': player, 'minute': [goal[1]], 'og': []})
             self.head['scores'] = str(home) + ":" + str(away)
             self.team1.score = home
-            self.team2.stats = away
+            self.team2.score = away
 
     def insertCardsStats(self, cards):
         for card in cards:
             playerData = self.teamOfFoundPlayer(card[0])
             player = playerData['player']
             team = playerData['team']
-            team.stats['cards'].append({'player': player, 'minute': card[1], 'color': card[2]})
+            team.stats['cards'].append({'player': player, 'minute': card[1], 'color': cardColor(card[2])})
 
     def insertSubStats(self, subs):
         for sub in subs:
@@ -105,21 +120,15 @@ class GameParser:
             team = playerData['team']
             team.stats['subs'].append({'minute': sub[1], 'out': sub[2].strip(), 'in': sub[3].strip()})
 
-
-team1 = stats.TeamStats()
-team2 = stats.TeamStats()
-
-parser = GameParser(team1, team2)
-
-with open("data2.txt", "r") as myFile:
-    data = myFile.read()
-
-parser.parse(data)
-team1.players()
-team1.get_goals()
-team1.get_cards()
-team1.get_subs()
-team2.players()
-team2.get_goals()
-team2.get_cards()
-team2.get_subs()
+    def print_summary(self):
+        if not self.parsed:
+            print("NO INFO AVAILABLE!")
+            return
+        print("HOME: " + self.head['home'])
+        print("AWAY: " + self.head['away'])
+        print("SCORE: " + self.head['scores'])
+        print("VENUE: " + self.head['venue'])
+        print("DATA: " + self.head['data'] + "\n")
+        self.team1.print_summary()
+        print("")
+        self.team2.print_summary()
